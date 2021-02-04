4b98c27b8c5a85798d794a8ffb6f4beeccb274ce
Drumil Patel <drumilpatel720@gmail.com>
2021-02-05 05:22:54 +0530

    Add basic skeleton for transformation from kingpin to kong
    Signed-off-by: Drumil Patel <drumilpatel720@gmail.com>

--- cmd/crossplane/main.go
+++ cmd/crossplane/main.go
@@ -17,44 +17,47 @@ limitations under the License.
 package main
 
 import (
-	"os"
-	"path/filepath"
+	"strings"
 
-	"gopkg.in/alecthomas/kingpin.v2"
-	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+	"github.com/alecthomas/kong"
 
-	"github.com/crossplane/crossplane-runtime/pkg/logging"
 	"github.com/crossplane/crossplane/cmd/crossplane/core"
 	"github.com/crossplane/crossplane/cmd/crossplane/rbac"
 )
 
+var _ = kong.Must(&cli)
+
+var cli struct {
+	Core core.Cmd `cmd:"" help:"Start core Crossplane controllers."`
+	Rbac rbac.Cmd `cmd:"" help:"Start Crossplane RBAC Manager controllers."`
+}
+
 func main() {
-	var (
-		app   = kingpin.New(filepath.Base(os.Args[0]), "An open source multicloud control plane.").DefaultEnvars()
-		debug = app.Flag("debug", "Run with debug logging.").Short('d').Bool()
-	)
 
-	c := core.FromKingpin(app.Command("core", "Start core Crossplane controllers.").Default())
-	r := rbac.FromKingpin(app.Command("rbac", "Start Crossplane RBAC Manager controllers."))
-	cmd := kingpin.MustParse(app.Parse(os.Args[1:]))
-
+	// TODO: Fix Debugging issue
 	// NOTE(negz): We must setup our logger after calling kingpin.MustParse in
 	// order to ensure the debug flag has been parsed and set.
-	zl := zap.New(zap.UseDevMode(*debug))
-	if *debug {
-		// The controller-runtime runs with a no-op logger by default. It is
-		// *very* verbose even at info level, so we only provide it a real
-		// logger when we're running in debug mode.
-		ctrl.SetLogger(zl)
-	}
+	// zl := zap.New(zap.UseDevMode(*debug))
+	// if *debug {
+	// 	// The controller-runtime runs with a no-op logger by default. It is
+	// 	// *very* verbose even at info level, so we only provide it a real
+	// 	// logger when we're running in debug mode.
+	// 	ctrl.SetLogger(zl)
+	// }
 
-	switch cmd {
-	case c.Name:
-		kingpin.FatalIfError(c.Run(logging.NewLogrLogger(zl.WithName("crossplane"))), "cannot run crossplane")
-	case r.Name:
-		kingpin.FatalIfError(r.Run(logging.NewLogrLogger(zl.WithName("rbac"))), "cannot run RBAC manager")
-	default:
-		kingpin.FatalUsage("unknown command %s", cmd)
-	}
+	ctx := kong.Parse(&cli,
+		kong.Name("crossplane"),
+		kong.Description("An open source multicloud control plane."),
+		kong.Vars{
+			"ManagementPolicyAll": rbac.ManagementPolicyAll,
+			"ManagementPolicyEnum": strings.Join(
+				[]string{
+					rbac.ManagementPolicyAll,
+					rbac.ManagementPolicyBasic,
+				},
+				","),
+		},
+		kong.UsageOnError())
+	err := ctx.Run()
+	ctx.FatalIfErrorf(err)
 }
